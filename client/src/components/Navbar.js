b49d687e1b65b39dc81aec35e92ac4cc8a6ebf90
Mia Stroud <miast36@gmail.com>
2021-07-08 14:53:28 -0400

    navbar is fixed position now

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,10 +3,12 @@ import {Link} from "react-router-dom"
 
 function Navbar(props) {
     return (
-        <nav>
-            <Link className="links" to="/read">finished</Link>
-            <Link className="links" to="/unread">unread</Link>
-            <button className="logout-btn" onClick={props.logout}>sign out</button>
+        <nav className="nav">
+            <div className='nav-container'>
+                <Link className="links" to="/read">finished</Link>
+                <Link className="links" to="/unread">unread</Link>
+                <button className="logout-btn" onClick={props.logout}>sign out</button>
+            </div>
         </nav>
     );
 }
