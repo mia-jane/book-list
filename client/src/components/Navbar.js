1dbe552f063784126fd431dd84ca5a8c869b87e8
Mia Stroud <miast36@gmail.com>
2021-07-08 21:59:39 -0400

    nav styling chnages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,10 +4,12 @@ import {Link} from "react-router-dom"
 function Navbar(props) {
     return (
         <nav className="nav">
-            <div className='nav-container'>
-                <Link className="links" to="/read">finished</Link>
-                <Link className="links" to="/unread">unread</Link>
-                <button className="logout-btn" onClick={props.logout}>sign out</button>
+            <div className="nav-backdrop">
+                <div className='nav-container'>
+                    <Link className="links" to="/read">finished</Link>
+                    <Link className="links" to="/unread">unread</Link>
+                    <button className="logout-btn" onClick={props.logout}>sign out</button>
+                </div>
             </div>
         </nav>
     );
